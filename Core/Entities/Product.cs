592fc5a5e422d1a69d1b8e6258c575ced1d61499
Huy Le <leduchuy271@gmail.com>
2024-11-25 20:10:29 +0700

    create entity product

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Core.Entities;
+
+public class Product : BaseEntity
+{   
+    public required String Name {get; set;}
+    public required String Description {get; set;}
+    public decimal Price {get; set;}
+    public required String ImgUrl {get; set;}
+    public required String Type {get; set;}
+    public required String Brand {get; set;}
+    public int QuantityInStock {get; set;}
+}
