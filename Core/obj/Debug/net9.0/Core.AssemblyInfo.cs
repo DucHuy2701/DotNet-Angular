f89b0309ec93065885d6efe0271a8c8f5b89a339
Huy Le <leduchuy271@gmail.com>
2024-11-30 20:27:08 +0700

    - create exception middleware

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06eee05d2aa8cbc5493ac0c10fa6a37486045807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be0f02fe911de2b5f1c9161531e7641416251c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
