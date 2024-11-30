ae193987d2029a674c3edcb30a0d07a47acb8b8a
Huy Le <leduchuy271@gmail.com>
2024-11-30 21:46:50 +0700

    - adding CORS

--- API/Program.cs
+++ API/Program.cs
@@ -16,11 +16,15 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 try
