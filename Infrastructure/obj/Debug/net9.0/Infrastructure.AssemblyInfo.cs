a115c43c1cce41e4987b0f2729d4720f2dc68214
Huy Le <leduchuy271@gmail.com>
2024-11-30 22:20:31 +0700

    summary pre-angular

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48589365a2f71ef4ccbb067d60df8a1ad90cd50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae193987d2029a674c3edcb30a0d07a47acb8b8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
