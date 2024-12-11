436fe84fc7e99d8a2754bafe2bc157bc4ffb2b70
Huy Le <leduchuy271@gmail.com>
2024-12-11 20:29:11 +0700

    - adding functions to cart - adding functions for quantity, remove, totals

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db0562d9d99a416c4f3b302b9f6ca6b794fab11e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf9a602cc27f60dbf3d6f9e5e1b6895cc362489")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
